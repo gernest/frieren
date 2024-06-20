59908a85ea9e3bc73a12ec9dcd3bdca15c79c331
gernest <geofreyernest@live.com>
2024-06-20 16:38:33 +0300

    match labels to extract

--- internal/predicate/predicate.go
+++ internal/predicate/predicate.go
@@ -1,6 +1,7 @@
 package predicate
 
 import (
+	"github.com/RoaringBitmap/roaring/roaring64"
 	"github.com/dgraph-io/badger/v4"
 	"github.com/gernest/frieren/internal/blob"
 	"github.com/gernest/frieren/internal/constants"
@@ -20,6 +21,7 @@ type Context struct {
 
 type Predicate interface {
 	Apply(ctx *Context) (*rows.Row, error)
+	Extract(_ *Context) (*roaring64.Bitmap, error)
 	Index() int
 }
 
@@ -29,8 +31,14 @@ type String struct {
 	op         traceql.Operator
 }
 
+var _ Predicate = (*String)(nil)
+
 func (f *String) Index() int {
 	return int(f.field) + int(f.op)
+}
+
+func (f *String) Extract(_ *Context) (*roaring64.Bitmap, error) {
+	return roaring64.New(), nil
 }
 
 func NewString(field constants.ID, op traceql.Operator, key, value string) *String {
@@ -63,8 +71,14 @@ type Int struct {
 	value uint64
 }
 
+var _ Predicate = (*Int)(nil)
+
 func (f *Int) Index() int {
 	return int(f.field) + int(f.op)
+}
+
+func (f *Int) Extract(_ *Context) (*roaring64.Bitmap, error) {
+	return roaring64.New(), nil
 }
 
 func (f *Int) Apply(ctx *Context) (*rows.Row, error) {
@@ -90,6 +104,8 @@ func NewInt(field constants.ID, op traceql.Operator, value uint64) *Int {
 }
 
 type And []Predicate
+
+var _ Predicate = (*And)(nil)
 
 func (f And) Index() int {
 	return 0
@@ -123,7 +139,37 @@ func (f And) Apply(ctx *Context) (*rows.Row, error) {
 	return r, nil
 }
 
+func (f And) Extract(ctx *Context) (*roaring64.Bitmap, error) {
+	if len(f) == 0 {
+		return roaring64.New(), nil
+	}
+	if len(f) == 1 {
+		return f[0].Extract(ctx)
+	}
+	r := roaring64.New()
+	for i := range f {
+		x, err := f[i].Extract(ctx)
+		if err != nil {
+			return nil, err
+		}
+		if x.IsEmpty() {
+			return x, nil
+		}
+		if i == 0 {
+			r = x
+			continue
+		}
+		r.And(x)
+		if r.IsEmpty() {
+			return r, nil
+		}
+	}
+	return r, nil
+}
+
 type Or []Predicate
+
+var _ Predicate = (*Or)(nil)
 
 func (f Or) Index() int {
 	return 0
@@ -147,6 +193,27 @@ func (f Or) Apply(ctx *Context) (*rows.Row, error) {
 			continue
 		}
 		r = r.Union(x)
+	}
+	return r, nil
+}
+func (f Or) Extract(ctx *Context) (*roaring64.Bitmap, error) {
+	if len(f) == 0 {
+		return roaring64.NewBitmap(), nil
+	}
+	if len(f) == 1 {
+		return f[0].Extract(ctx)
+	}
+	r := roaring64.New()
+	for i := range f {
+		x, err := f[i].Extract(ctx)
+		if err != nil {
+			return nil, err
+		}
+		if i == 0 {
+			r = x
+			continue
+		}
+		r.Or(x)
 	}
 	return r, nil
 }
