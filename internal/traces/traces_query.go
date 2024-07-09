82ad7968548b1b02f899bb44f3fd35d8b8f3c2fa
gernest <geofreyernest@live.com>
2024-07-09 12:53:24 +0300

    update find trace by id

--- internal/traces/traces_query.go
+++ internal/traces/traces_query.go
@@ -5,7 +5,7 @@ import (
 	"encoding/hex"
 	"time"
 
-	"github.com/gernest/rbf"
+	"github.com/gernest/frieren/internal/lbx"
 	"github.com/gernest/rbf/dsl/bsi"
 	"github.com/gernest/rbf/dsl/mutex"
 	rq "github.com/gernest/rbf/dsl/query"
@@ -36,8 +36,9 @@ func (db *Store) FindTraceByID(ctx context.Context, req *tempopb.TraceByIDReques
 	}
 
 	m := make(map[uint64]map[uint64][]*tempov1.Span)
-	scope := map[uint64]*commonv1.InstrumentationScope{}
-	resource := map[uint64]*resourcev1.Resource{}
+
+	otlpScope := map[uint64]*commonv1.InstrumentationScope{}
+	otlpResourrce := map[uint64]*resourcev1.Resource{}
 
 	shards := r.Range(time.Unix(0, timeStart), time.Unix(0, timeEnd))
 	for i := range shards {
@@ -56,23 +57,35 @@ func (db *Store) FindTraceByID(ctx context.Context, req *tempopb.TraceByIDReques
 			if f.IsEmpty() {
 				return nil
 			}
+			resource, err := txn.Tx.Cursor(txn.Key("resource"))
+			if err != nil {
+				return err
+			}
+			defer resource.Close()
+			scope, err := txn.Tx.Cursor(txn.Key("scope"))
+			if err != nil {
+				return err
+			}
+			defer scope.Close()
+
+			span, err := txn.Tx.Cursor(txn.Key("span"))
+			if err != nil {
+				return err
+			}
+			defer span.Close()
+
+			data := lbx.NewData(f.Columns())
 
 			// read resource
 			resourceMapping := map[uint64]uint64{}
-			err = txn.Cursor("resource", func(c *rbf.Cursor, tx *tx.Tx) error {
-				return mutex.Extract(c, tx.Shard, f, func(column, value uint64) error {
-					if _, seen := resource[value]; seen {
-						return nil
-					}
-					resourceMapping[column] = value
-					o := resourcev1.Resource{}
-					err := o.Unmarshal(tx.Tr.Blob("resource", value))
-					if err != nil {
-						return err
-					}
-					resource[value] = &o
-					return nil
-				})
+			err = lbx.BSI(data, resource, f, txn.Shard, func(column uint64, value int64) {
+				resourceMapping[column] = uint64(value)
+				if _, seen := otlpResourrce[uint64(value)]; seen {
+					return
+				}
+				o := resourcev1.Resource{}
+				o.Unmarshal(txn.Tr.Blob("resource", uint64(value)))
+				otlpResourrce[uint64(value)] = &o
 			})
 			if err != nil {
 				return err
@@ -80,41 +93,28 @@ func (db *Store) FindTraceByID(ctx context.Context, req *tempopb.TraceByIDReques
 
 			// read scope
 			scopeMapping := map[uint64]uint64{}
-			err = txn.Cursor("scope", func(c *rbf.Cursor, tx *tx.Tx) error {
-				return mutex.Extract(c, tx.Shard, f, func(column, value uint64) error {
-					scopeMapping[column] = value
-					if _, seen := scope[value]; seen {
-						return nil
-					}
-					o := commonv1.InstrumentationScope{}
-					err := o.Unmarshal(tx.Tr.Blob("scope", value))
-					if err != nil {
-						return err
-					}
-					scope[value] = &o
-					return nil
-				})
+			err = lbx.BSI(data, scope, f, txn.Shard, func(column uint64, value int64) {
+				scopeMapping[column] = uint64(value)
+				if _, seen := otlpScope[uint64(value)]; seen {
+					return
+				}
+				o := commonv1.InstrumentationScope{}
+				o.Unmarshal(txn.Tr.Blob("scope", uint64(value)))
+				otlpScope[uint64(value)] = &o
 			})
 			if err != nil {
 				return err
 			}
-			// read span
-			return txn.Cursor("span", func(c *rbf.Cursor, tx *tx.Tx) error {
-				return mutex.Extract(c, tx.Shard, f, func(column, value uint64) error {
-					rs, ok := m[resourceMapping[column]]
-					if !ok {
-						rs = make(map[uint64][]*tempov1.Span)
-						m[resourceMapping[column]] = rs
-					}
-					sid := scopeMapping[column]
-					o := tempov1.Span{}
-					err := o.Unmarshal(tx.Tr.Blob("scope", value))
-					if err != nil {
-						return err
-					}
-					rs[sid] = append(rs[sid], &o)
-					return nil
-				})
+			return lbx.BSI(data, span, f, txn.Shard, func(column uint64, value int64) {
+				rs, ok := m[resourceMapping[column]]
+				if !ok {
+					rs = make(map[uint64][]*tempov1.Span)
+					m[resourceMapping[column]] = rs
+				}
+				sid := scopeMapping[column]
+				o := tempov1.Span{}
+				o.Unmarshal(txn.Tr.Blob("span", uint64(value)))
+				rs[sid] = append(rs[sid], &o)
 			})
 		})
 	}
@@ -122,18 +122,18 @@ func (db *Store) FindTraceByID(ctx context.Context, req *tempopb.TraceByIDReques
 	// Assemble result
 	result := &tempopb.TraceByIDResponse{
 		Trace: &tempopb.Trace{
-			Batches: make([]*tempov1.ResourceSpans, 0, len(resource)),
+			Batches: make([]*tempov1.ResourceSpans, 0, len(otlpResourrce)),
 		},
 	}
 
-	for ri, r := range resource {
+	for ri, r := range otlpResourrce {
 		xr := &tempov1.ResourceSpans{
 			Resource:   r,
 			ScopeSpans: make([]*tempov1.ScopeSpans, 0, len(m[ri])),
 		}
 		for si, sp := range m[ri] {
 			xr.ScopeSpans = append(xr.ScopeSpans, &tempov1.ScopeSpans{
-				Scope: scope[si],
+				Scope: otlpScope[si],
 				Spans: sp,
 			})
 		}
