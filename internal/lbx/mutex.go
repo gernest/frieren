1353c4dd4bd318441422e1572f2913bb563284ee
gernest <geofreyernest@live.com>
2024-07-10 07:53:44 +0300

    fix labels reading

--- internal/lbx/mutex.go
+++ internal/lbx/mutex.go
@@ -6,26 +6,33 @@ import (
 	"github.com/gernest/rbf"
 	"github.com/gernest/rbf/dsl/cursor"
 	"github.com/gernest/rbf/dsl/tx"
+	"github.com/gernest/roaring"
 	"github.com/prometheus/prometheus/model/labels"
 )
 
 var sep = []byte("=")
 
 func Labels(c *rbf.Cursor, field string, tx *tx.Tx, rowID uint64) (labels.Labels, error) {
-	r, err := cursor.Row(c, tx.Shard, rowID)
+	var rows []uint64
+	err := cursor.Rows(c, 0, func(row uint64) error {
+		rows = append(rows, row)
+		return nil
+	}, roaring.NewBitmapColumnFilter(rowID))
 	if err != nil {
 		return nil, err
 	}
-	if r.IsEmpty() {
+	if len(rows) == 0 {
 		return labels.Labels{}, nil
 	}
-	b := labels.NewScratchBuilder(int(r.Count()))
-	r.RangeColumns(func(u uint64) error {
-		name, value, _ := bytes.Cut(tx.Tr.Key(field, u), sep)
-		b.Add(string(name), string(value))
-		return nil
+	rs := make(labels.Labels, 0, len(rows))
+	tx.Tr.Keys(field, rows, func(value []byte) {
+		name, value, _ := bytes.Cut(value, sep)
+		rs = append(rs, labels.Label{
+			Name:  string(name),
+			Value: string(value),
+		})
 	})
-	return b.Labels(), nil
+	return rs, nil
 }
 
 func Blobs(c *rbf.Cursor, field string, tx *tx.Tx, rowID uint64) (labels.Labels, error) {
